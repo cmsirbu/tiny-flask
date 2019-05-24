73da8c97289e184c97227f2011ce906166d11feb
cristian@twoflower <cmsirbu@users.noreply.github.com>
2019-05-24 22:10:11 +0100

    add travis tests

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from tinyflask import app
+
+def test_http_root():
+    """
+    Test the / path works and returns the expected template.
+    """
+
+    response = app.test_client().get("/")
+
+    rdata = response.data.decode("utf-8")
+
+    assert response.status_code == 200
+    assert "It's alive!" in rdata
